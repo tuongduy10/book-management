970e115d05cf715b6be2007b18f5207e416595e4
tuongduy10 <vtngduy101120@gmail.com>
2023-01-07 00:16:53 +0700

    add template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Paperbase from './_components/paper-base'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Paperbase />
 }
